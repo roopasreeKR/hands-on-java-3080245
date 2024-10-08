4958a198a12c107e655fc39337e3d0b672b49836
roopasreeKR <84180468+roopasreeKR@users.noreply.github.com>
2024-10-08 13:42:37 +0000

    Customer changes

---
+++ src/main/java/bank/Customer.java
@@ -0,0 +1,60 @@
+package bank;
+
+public class Customer {
+
+  private int ID;
+  private String name;
+  private String userName;
+  private String password;
+  private int accountID;
+
+  public Customer(int ID, String name, String userName, String password, int accountID) {
+    setID(ID);
+    setName(name);
+    setUserName(userName);
+    setPassword(password);
+    setAccountID(accountID);
+
+  }
+
+  public int getID() {
+    return this.ID;
+  }
+
+  public void setID(int ID) {
+    this.ID = ID;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getUserName() {
+    return this.userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public int getAccountID() {
+    return this.accountID;
+  }
+
+  public void setAccountID(int accountID) {
+    this.accountID = accountID;
+  }
+
+}
